37c5c08e2495ac3fff8523a1b9687f94d5629312
Hatice Kandemir <kandemirhatice123@gmail.com>
2025-04-03 00:19:37 +0300

    editing

--- blogapp/blogapp/settings.py
+++ blogapp/blogapp/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-iwu28o!9pdu)0hcj9n@a7)*g7slu)x)^r1wpm!zwv@re$*v^5w'
+
 # SECRET_KEY = getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = getenv("IS_DEVELOPMENT", True)
